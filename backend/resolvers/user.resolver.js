db405cc2dd8b2495f29455a40a65a03387a1e52d
Steven Gong <gonghaima@hotmail.com>
2024-08-17 17:00:38 +1000

    get user by id

--- backend/resolvers/user.resolver.js
+++ backend/resolvers/user.resolver.js
@@ -82,14 +82,17 @@ const userResolver = {
         throw new Error('Internal server error');
       }
     },
+    // user: async (_, { userId }) => {
+    //   try {
+    //     const user = await User.findById(userId);
+    //     return user;
+    //   } catch (err) {
+    //     console.error('Error in user query:', err);
+    //     throw new Error(err.message || 'Error getting user');
+    //   }
+    // },
     user: async (_, { userId }) => {
-      try {
-        const user = await User.findById(userId);
-        return user;
-      } catch (err) {
-        console.error('Error in user query:', err);
-        throw new Error(err.message || 'Error getting user');
-      }
+      return users.find((user) => user._id === userId);
     },
     users: () => {
       return users;

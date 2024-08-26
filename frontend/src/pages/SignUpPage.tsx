9284cf496391b02524deed74884861bbd94b13da
Steven Gong <gonghaima@hotmail.com>
2024-08-26 14:32:49 +1000

    signup page

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-// import RadioButton from "../components/RadioButton";
-// import InputField from "../components/InputField";
+import RadioButton from '../components/RadioButton';
+// import InputField from '../components/InputField';
+import InputField from '../components/InputField';
 
 const SignUpPage = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [signUpData, setSignUpData] = useState({
     name: '',
     username: '',
@@ -12,21 +12,21 @@ const SignUpPage = () => {
     gender: '',
   });
 
-  // const handleChange = (e) => {
-  // 	const { name, value, type } = e.target;
+  const handleChange = (e) => {
+    const { name, value, type } = e.target;
 
-  // 	if (type === "radio") {
-  // 		setSignUpData((prevData) => ({
-  // 			...prevData,
-  // 			gender: value,
-  // 		}));
-  // 	} else {
-  // 		setSignUpData((prevData) => ({
-  // 			...prevData,
-  // 			[name]: value,
-  // 		}));
-  // 	}
-  // };
+    if (type === 'radio') {
+      setSignUpData((prevData) => ({
+        ...prevData,
+        gender: value,
+      }));
+    } else {
+      setSignUpData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+    }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -45,47 +45,47 @@ const SignUpPage = () => {
               Join to keep track of your expenses
             </h1>
             <form className="space-y-4" onSubmit={handleSubmit}>
-              {/* <InputField
-								label='Full Name'
-								id='name'
-								name='name'
-								value={signUpData.name}
-								onChange={handleChange}
-							/>
-							<InputField
-								label='Username'
-								id='username'
-								name='username'
-								value={signUpData.username}
-								onChange={handleChange}
-							/>
+              <InputField
+                label="Full Name"
+                id="name"
+                name="name"
+                value={signUpData.name}
+                onChange={handleChange}
+              />
+              <InputField
+                label="Username"
+                id="username"
+                name="username"
+                value={signUpData.username}
+                onChange={handleChange}
+              />
 
-							<InputField
-								label='Password'
-								id='password'
-								name='password'
-								type='password'
-								value={signUpData.password}
-								onChange={handleChange}
-							/>
-							<div className='flex gap-10'>
-								<RadioButton
-									id='male'
-									label='Male'
-									name='gender'
-									value='male'
-									onChange={handleChange}
-									checked={signUpData.gender === "male"}
-								/>
-								<RadioButton
-									id='female'
-									label='Female'
-									name='gender'
-									value='female'
-									onChange={handleChange}
-									checked={signUpData.gender === "female"}
-								/>
-							</div> */}
+              <InputField
+                label="Password"
+                id="password"
+                name="password"
+                type="password"
+                value={signUpData.password}
+                onChange={handleChange}
+              />
+              <div className="flex gap-10">
+                <RadioButton
+                  id="male"
+                  label="Male"
+                  name="gender"
+                  value="male"
+                  onChange={handleChange}
+                  checked={signUpData.gender === 'male'}
+                />
+                <RadioButton
+                  id="female"
+                  label="Female"
+                  name="gender"
+                  value="female"
+                  onChange={handleChange}
+                  checked={signUpData.gender === 'female'}
+                />
+              </div>
 
               <div>
                 <button
